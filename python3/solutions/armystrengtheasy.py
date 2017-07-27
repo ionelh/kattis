364fd20f8b77ed0368081ff2c06a8d70d8c7c726
ionelh <ionel.hindorean@gmail.com>
2017-07-27 09:55:07 +0300

    add py solutions for armystrength easy and hard

---
+++ python3/solutions/armystrengtheasy.py
@@ -0,0 +1,15 @@
+# Problem: https://open.kattis.com/problems/armystrengtheasy
+
+import sys
+# Cmd + D to print the results in the console
+
+input = sys.stdin.readlines()
+no_tc = int(input[0])
+
+for i in range(no_tc):
+  g_army = input[4 * i + 3].split()
+  m_army = input[4 * i + 4].split()
+
+  max_g = max([int(x) for x in g_army])
+  max_m = max([int(x) for x in m_army])
+  print('MechaGodzilla' if max_g < max_m else 'Godzilla')
